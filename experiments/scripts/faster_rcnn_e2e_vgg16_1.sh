6acf2edd70096037fdf8468291484a782b34cff5
Ali Aliev <alien-box@yandex.ru>
2017-10-03 10:58:53 +0300

    fix model training

--- experiments/scripts/faster_rcnn_e2e_vgg16_1.sh
+++ experiments/scripts/faster_rcnn_e2e_vgg16_1.sh
@@ -9,9 +9,9 @@ GPU_ID=$1
 
 EXP_ID=1
 TRAIN_IMDB="nexar2_train_40k"
-TRAIN_ITERS=80000
+TRAIN_ITERS=400000
 SOLVER=models/nexar2/VGG16/faster_rcnn_end2end/solver_1.prototxt
-WEIGHTS=output/faster_rcnn_end2end/nexar2_train_40k/vgg16_faster_rcnn_iter_10000.caffemodel # trained with lr=1e-3
+WEIGHTS=data/faster_rcnn_models/VGG16_faster_rcnn_final.caffemodel
 CONFIG=experiments/cfgs/faster_rcnn_end2end_nexar2.yml
 
 LOG="experiments/logs/faster_rcnn_end2end_${EXP_ID}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
